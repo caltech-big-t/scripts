c11007ecc362fe3247ab26f5e229c9c385b33377
Eric <ericc3141@users.noreply.github.com>
2020-06-21 20:43:03 -0700

    parse balfour files

---
+++ facepalm/face/src/Main.elm
@@ -0,0 +1,109 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Element exposing (Element, text)
+import Element.Input as Input
+import File exposing (File)
+import File.Select as Select
+import Html exposing (Html)
+import Peeps exposing (Peep)
+import Task
+
+
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { peeps : Maybe (Result (List Peeps.Error) (List Peep)) }
+
+
+type Msg
+    = Select
+    | Selected File
+    | Loaded String
+
+
+init : () -> ( Model, Cmd Msg )
+init =
+    always ( { peeps = Nothing }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions =
+    always Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Select ->
+            ( model, Select.file [ "text/txt" ] Selected )
+
+        Selected file ->
+            ( model, Task.perform Loaded (File.toString file) )
+
+        Loaded contents ->
+            ( { model | peeps = Just <| Peeps.fromBalfour contents }, Cmd.none )
+
+
+view : Model -> Document Msg
+view model =
+    let
+        peepsView =
+            case model.peeps of
+                Nothing ->
+                    text "Import file to get started"
+
+                Just (Err errs) ->
+                    text "errors importing"
+
+                Just (Ok peeps) ->
+                    viewPeeps peeps
+
+        body =
+            [ text "Facepalm"
+            , Input.button [] { onPress = Just Select, label = text "Import" }
+            , peepsView
+            ]
+    in
+    { title = "FacePalm"
+    , body = [ Element.layout [] <| Element.column [] body ]
+    }
+
+
+viewPeeps : List Peep -> Element Msg
+viewPeeps peeps =
+    let
+        pics =
+            { header = text "Pic"
+            , width = Element.shrink
+            , view = \peep -> text peep.pic
+
+            --, view = \peep -> Element.image [] { src = peep.pic, description = "" }
+            }
+
+        lastnames =
+            { header = text "Last"
+            , width = Element.fill
+            , view = \peep -> text peep.lastname
+            }
+
+        firstnames =
+            { header = text "First"
+            , width = Element.fill
+            , view = \peep -> text peep.firstname
+            }
+
+        grades =
+            { header = text "Grade"
+            , width = Element.fill
+            , view = \peep -> text peep.grade
+            }
+    in
+    Element.table [] { data = peeps, columns = [ pics, lastnames, firstnames, grades ] }
---
+++ facepalm/face/src/Peeps.elm
@@ -0,0 +1,78 @@
+module Peeps exposing (Error, Peep, fromBalfour)
+
+import Csv
+import Parser exposing (DeadEnd)
+import Result exposing (Result(..))
+
+
+type alias Peep =
+    { lastname : String
+    , firstname : String
+    , grade : String
+    , pic : String
+    }
+
+
+type Error
+    = BadParse Int
+    | Missing Int String
+
+
+listIdx : Int -> List a -> Maybe a
+listIdx i lst =
+    List.head <| List.drop i lst
+
+
+fromRecord : List String -> Result String Peep
+fromRecord record =
+    let
+        lastname =
+            listIdx 4 record
+
+        firstname =
+            case listIdx 6 record of
+                Just "" ->
+                    listIdx 5 record
+
+                name ->
+                    name
+
+        grade =
+            listIdx 3 record
+
+        pic =
+            listIdx 2 record
+    in
+    case Maybe.map4 Peep lastname firstname grade pic of
+        Just peep ->
+            Ok peep
+
+        Nothing ->
+            Err "TODO"
+
+
+fromBalfour : String -> Result (List Error) (List Peep)
+fromBalfour csv =
+    case Csv.parseWith '\t' csv of
+        Err deadends ->
+            Err <| List.map (\deadend -> BadParse deadend.row) deadends
+
+        Ok { headers, records } ->
+            let
+                partition : Result String Peep -> ( List Peep, List Error, Int ) -> ( List Peep, List Error, Int )
+                partition res ( succeeded_, failed_, i ) =
+                    case res of
+                        Ok peep ->
+                            ( peep :: succeeded_, failed_, i + 1 )
+
+                        Err msg ->
+                            ( succeeded_, Missing i msg :: failed_, i + 1 )
+
+                ( succeeded, failed, _ ) =
+                    List.foldl partition ( [], [], 0 ) (List.map fromRecord records)
+            in
+            if List.length failed == 0 then
+                Ok succeeded
+
+            else
+                Err failed
